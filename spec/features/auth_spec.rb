a4629023862df3a5e6a042432bfd0981c42fd506
Mello <dan.mello1001@gmail.com>
2014-11-07 12:02:45 -0700

    fixed bug

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,15 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'Users can signup' do
+    visit root_path
+    click_on "Register"
+    fill_in "Email", with: "test@test.com"
+    fill_in "Name", with: "Test Test"
+    fill_in "Password", with: "test"
+    fill_in "Confirm", with: "test"
+    within(".registration-form") {click_on "Register"}
+    expect(page).to have_content("test@test.com")
+  end
+
 end
